45042c3a986c126c335329ca05ec0e28747afca8
Marcos Machado <marcoseu1994@gmail.com>
2020-12-02 22:57:30 -0300

    Ajustes no endpoint /categorias/{id}

---
+++ src/main/java/com/marcos/cursospring/services/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,14 @@
+package com.marcos.cursospring.services.exceptions;
+
+public class ObjectNotFoundException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+	
+	public ObjectNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+	}
+}
